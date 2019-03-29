4dd2647e2a625b8c3d9c2f147372c7b981708010
shoNagai <strawberry4062@gmail.com>
2019-03-29 13:58:05 +0900

    add zilliqajs & contract

---
+++ migrations/deploy.js
@@ -0,0 +1,101 @@
+const { Transaction } = require('@zilliqa-js/account');
+const { BN, Long, bytes, units } = require('@zilliqa-js/util');
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
+const CP = require('@zilliqa-js/crypto');
+
+const fs = require('fs');
+
+const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
+
+const CHAIN_ID = 333; // Developer testnet
+const MSG_VERSION = 1;
+const VERSION = bytes.pack(CHAIN_ID, MSG_VERSION);
+
+// Populate the wallet with an account
+privkey = '688c37aa9ade9a0a18706feeeab87b663013f7b624f1d5aa1bffb24e0a5e0558';
+
+zilliqa.wallet.addByPrivateKey(
+  privkey
+);
+
+const address = CP.getAddressFromPrivateKey(privkey);
+console.log("Your account address is:");
+console.log(`0x${address}`);
+
+async function testBlockchain() {
+  try {
+
+    // Get Balance
+    const balance = await zilliqa.blockchain.getBalance(address);
+    console.log(`Your account balance is:`);
+    console.log(balance.result)
+    const myGasPrice = units.toQa('1000', units.Units.Li); // Gas Price that will be used by all transactions
+
+    // get contract code
+    const code = fs.readFileSync('contracts/HelloWorld.scilla', 'utf-8');
+
+    const init = [
+      // this parameter is mandatory for all init arrays
+      {
+        vname: "_scilla_version",
+        type: "Uint32",
+        value: "0"
+      },
+      {
+        vname: "owner",
+        type: "ByStr20",
+        // NOTE: all byte strings passed to Scilla contracts _must_ be
+        // prefixed with 0x. Failure to do so will result in the network
+        // rejecting the transaction while consuming gas!
+        value: `0x${address}`
+      }
+    ];
+
+    // Instance of class Contract
+    const contract = zilliqa.contracts.new(code, init);
+
+    //Deploy the contract
+    const [deployTx, hello] = await contract.deploy({
+      version: VERSION,
+      gasPrice: myGasPrice,
+      gasLimit: Long.fromNumber(10000)
+    });
+
+    //Introspect the state of the underlying transaction
+    console.log(`Deployment Transaction ID: ${deployTx.id}`);
+    console.log(`Deployment Transaction Receipt:`);
+    console.log(deployTx.txParams.receipt);
+
+    //Get the deployed contract address
+    console.log("The contract address is:");
+    console.log(hello.address);
+    const callTx = await hello.call(
+      "setHello",
+      [
+        {
+          vname: "msg",
+          type: "String",
+          value: "Hello World"
+        }
+      ],
+      {
+        // amount, gasPrice and gasLimit must be explicitly provided
+        version: VERSION,
+        amount: new BN(0),
+        gasPrice: myGasPrice,
+        gasLimit: Long.fromNumber(8000),
+      }
+    );
+    console.log(callTx);
+
+    //Get the contract state
+    const state = await hello.getState();
+    console.log("The state of the contract is:");
+    console.log(state);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+testBlockchain();
